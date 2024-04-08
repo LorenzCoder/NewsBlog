b9fd3811a9f274b5b9cef115974eaa5c3b7d29c7
LorenzCoder <136112857+LorenzCoder@users.noreply.github.com>
2024-04-08 16:46:22 +0200

    Add files via upload
    1.0.1
    Funktioniert leider noch nicht ganz. Die Meldungsseite und die Meldungsdetailseite funktionieren, aber wenn man keine Parameter an die URL anhängt kommt ein Fehler.

--- meinPythonProjekt/news/urls.py
+++ meinPythonProjekt/news/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Home'),
+    #path('password_reset_form/', views.password_reset_form, name='Password Reset'),
     path('meldungen/', views.meldungen, name='meldungen'),
     path('meldungen/<int:meldungs_id>/', views.meldung_detail, name='meldung_detail'),
 ]
