8d23b3b1343901729e2e2390d54d43eae66ef11e
Arthur Krenmueller <arthurkrenmueller@Anitas-iMac-2.local>
2024-05-10 20:06:19 +0200

    Updated SignUp Function

--- meinPythonProjekt/meinPythonProjekt/urls.py
+++ meinPythonProjekt/meinPythonProjekt/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
     path('', include(('news.urls', 'news'), namespace='news')),
+    path('', TemplateView.as_view(template_name="news/home.html"), name="home"),
 ]
--- meinPythonProjekt/news/views.py
+++ meinPythonProjekt/news/views.py
@@ -30,7 +30,6 @@ def index(request):
 
 
 def signup(request):
-    template = loader.get_template('registration/signup.html')
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
@@ -39,4 +38,4 @@ def signup(request):
             return redirect('home')
     else:
         form = SignupForm()
-    return render(request, template, {'form': form})
+        return render(request, 'registration/signup.html', {'form': form})
