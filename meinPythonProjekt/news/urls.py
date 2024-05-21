6d6f3f65daa5a83c234d11f8ebcd593853073d36
LorenzCoder <lorenzbarta25@proton.me>
2024-05-21 14:27:03 +0200

    1.4.0
    Details siehe 'VersionsHistory.txt' unter dem Punkt '1.4.0'

--- meinPythonProjekt/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('meldungen/', views.meldungen, name='meldungen'),
-    path('meldungen/<int:meldungs_id>/', views.meldung_detail, name='meldung_detail'),
-]
---
+++ software/meinPythonProjekt/news/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Meldung(models.Model):
+    titel = models.CharField(max_length=100)
+    zeitstempel = models.DateTimeField()
+    text = models.TextField('Meldungstext')
+    class Admin:
+        pass
+    def __unicode__(self):
+        return self.titel
+
+class Kommentar(models.Model):
+    meldung = models.ForeignKey(Meldung, on_delete=models.CASCADE)
+    autor = models.CharField(max_length=70)
+    text = models.TextField('Kommentartext')
+    def __unicode__(self):
+        return self.titel
