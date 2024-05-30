0dada627b23c1f64259cdac60710024c49505153
LorenzCoder <lorenzbarta25@proton.me>
2024-05-30 13:01:00 +0200

    .

--- software/meinPythonProjekt/news/forms.py
+++ software/meinPythonProjekt/news/forms.py
@@ -1,15 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, get_user_model, password_validation
-from django.contrib.auth import *
-
-
-class SignupForm(forms.Form):
-    email = forms.TextField(max_length=200, help_text='Required')
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=255)
--- software/meinPythonProjekt/news/urls.py
+++ software/meinPythonProjekt/news/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('signup/', views.signup, name='signup'),
     path('meldungen/', views.meldungen, name='meldungen'),
     path('meldungen/<int:meldungs_id>/', views.meldung_detail, name='meldung_detail'),
 ]
