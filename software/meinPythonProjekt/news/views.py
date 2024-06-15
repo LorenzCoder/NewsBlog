440363b83796c085ab14f7b856caa25c3f137f0e
LorenzCoder <lorenzbarta25@proton.me>
2024-06-15 14:03:29 +0200

    1.5.0
    Details siehe 'VersionsHistory.txt' unter dem Punkt '1.5.0'

--- software/meinPythonProjekt/news/views.py
+++ software/meinPythonProjekt/news/views.py
@@ -27,15 +27,3 @@ def index(request):
         'request': request,
     }
     return HttpResponse(template.render(context))
-
-
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignupForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('home')
-#     else:
-#         form = SignupForm()
-#         return render(request, 'registration/signup.html', {'form': form})
